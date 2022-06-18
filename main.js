fbe1e2d918d46763865a55987d5e0ce789ab1a0d
SeungWon_Lee <97611692+SeungWonE@users.noreply.github.com>
2022-06-18 23:56:10 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 
+function modalToggle(){
+    var modal = document.getElementById('modal');
+    alert(modal.style.display);
+    if(modal.style.display =='none'){
+        modal.style.display = 'show';
+    }
+    else{
+        modal.style.display = 'none';
+    }
+}
+
+var modalInButton = document.getElementById('modalIn');
+var modalOutButton = document.getElementById('modalOut');
+
+modalInButton.addEventListener("click", modalToggle());
+modalOutButton.addEventListener("click", modalToggle());
